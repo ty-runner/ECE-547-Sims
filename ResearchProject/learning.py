be34b6238dd4ad6ad423148c87203379a9ecfc2d
ty-runner <ty.runner02@gmail.com>
2023-11-22 14:52:30 -0500

    Implement start on Decision tree boosting

--- ResearchProject/learning.py
+++ ResearchProject/learning.py
@@ -1,80 +1,30 @@
-# # Import necessary libraries
-# import numpy as np
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import accuracy_score
-
-# # Step 1: Understand the Problem
-# # - Assume you have a dataset with network traffic features and labels indicating congestion.
-
-# # Step 2: Data Collection
-# # - Load your dataset (replace 'your_dataset.csv' with the actual file path).
-# data = pd.read_csv('your_dataset.csv')
-
-# # Step 3: Feature Engineering
-# # - Assume 'throughput', 'latency', and 'packet_loss' are relevant features.
-# features = data[['throughput', 'latency', 'packet_loss']]
-
-# # Step 4: Labeling
-# # - Assume 'congested' is the label indicating congestion.
-# labels = data['congested']
-
-# # Step 5: Split into Training and Testing Sets
-# features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2, random_state=42)
-
-# # Step 6: Model Selection and Training
-# # - Use a RandomForestClassifier as an example model.
-# model = RandomForestClassifier(n_estimators=100, random_state=42)
-# model.fit(features_train, labels_train)
-
-# # Step 7: Integration with Congestion Control Mechanism
-# # - In a real-world scenario, you would integrate the model with the congestion control mechanism.
-
-# # Step 8: Real-time Monitoring
-# # - Continuously monitor network data and feed it into the trained model.
-
-# # Step 9: Evaluation and Tuning
-# # - Evaluate the model on the testing set.
-# predictions = model.predict(features_test)
-# accuracy = accuracy_score(labels_test, predictions)
-# print(f'Model Accuracy: {accuracy}')
-
-# # Step 10: Deployment (Not shown in this example)
-# # - Deploy the model in a controlled environment and gradually scale up.
-
-# # Step 11: Continuous Improvement (Not shown in this example)
-# # - Periodically update the model with new data for continuous improvement.
-
-
 # Import necessary libraries
-from sklearn.datasets import load_iris
+from sklearn.ensemble import AdaBoostRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
+import pandas as pd
 
-# Load the Iris dataset
-iris = load_iris()
-X = iris.data
-y = iris.target
+# Load your dataset (replace 'your_dataset.csv' with your actual dataset)
+data = pd.read_csv('your_dataset.csv')
 
-# Split the dataset into training and testing sets
+# Assume the target variable is 'congestion_level' and features are all other columns
+X = data.drop('congestion_level', axis=1)
+y = data['congestion_level']
+
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a Decision Tree classifier (as a weak learner)
-base_classifier = DecisionTreeClassifier(max_depth=1)
+# Instantiate an AdaBoost regressor with DecisionTreeRegressor as the base estimator
+# You can use DecisionTreeClassifier for classification problems
+base_estimator = None  # Use DecisionTreeRegressor as the default base estimator
+model = AdaBoostRegressor(base_estimator=base_estimator, random_state=42)
 
-# Create an AdaBoost classifier with 50 weak learners
-adaboost_classifier = AdaBoostClassifier(base_classifier, n_estimators=50, random_state=42)
-
-# Train the AdaBoost classifier
-adaboost_classifier.fit(X_train, y_train)
+# Train the model
+model.fit(X_train, y_train)
 
 # Make predictions on the test set
-predictions = adaboost_classifier.predict(X_test)
+y_pred = model.predict(X_test)
 
-# Evaluate the accuracy of the model
-accuracy = accuracy_score(y_test, predictions)
-print(f'Accuracy: {accuracy:.2f}')
-
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
